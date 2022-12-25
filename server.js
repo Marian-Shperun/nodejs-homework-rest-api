007506cf51b32c7fdc8c13283a294547c7e581a2
Marian-Shperun <97259432+Marian-Shperun@users.noreply.github.com>
2022-12-25 21:42:18 +0100

    final corrections

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const app = require("./app");
+const { dbStart } = require("./db/connection.js");
 
-const PORT = 3000;
+const dotenv = require("dotenv");
 
-app.listen(3000, () => {
-  console.log(`Server running. Use our API on port: http://localhost:${PORT}/`);
-});
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const CONNECTION_URL = process.env.CONNECTION_URL;
+
+dbStart(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(
+        `Server running. Use our API on port: http://localhost:${PORT}/`
+      )
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
